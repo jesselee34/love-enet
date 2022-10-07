efd2db741c7016fb554d3e358b3e04e798d1c729
Jesse Schmucker <Jesse@Marissas-MacBook-Pro.local>
2022-10-07 18:19:01 -0500

    Attempted actual linear interpolation

--- client/main.lua
+++ client/main.lua
@@ -10,8 +10,11 @@ local keys = {}
 local event
 
 local DT = 0
+-- rdt is the time since the last 1/60 tick.
+local rdt = 0
 
-local state = {100, 100}
+local state = {}
+local currentState
 
 local function join (tbl)
   local result = ''
@@ -37,6 +40,17 @@ local function parse (str)
   return result
 end
 
+local function interpolate (rdt, pt1, pt2)
+  local x1 = pt1[1]
+  local y1 = pt1[2]
+  local x2 = pt2[1]
+  local y2 = pt2[2]
+  local x = (x1 - x2) * (rdt/(1/60))
+  local y = y1 + ((x - x1) / (x2 - x1)) * (y2 - y1)
+
+  return { x, y }
+end
+
 function love.load(args)
 	-- establish a connection to host on same PC
 	host = enet.host_create()
@@ -56,7 +70,10 @@ function love.update(dt)
   end
   
   timer = timer + DT
-  event = host:service()
+  
+  local status, error = pcall(function ()
+    event = host:service()
+  end)
 
   if timer >= tick then
     timer = 0
@@ -64,9 +81,26 @@ function love.update(dt)
     keys = {}
   end
 
+  if currentState then
+    rdt = rdt + DT
+
+    if rdt >= 1/60 then
+      rdt = 1/60 - rdt
+      table.remove(state, 1)
+    end
+
+    -- TODO: Make sure state[2] exists need to pause
+
+    currentState = interpolate(rdt, state[1], state[2])
+  end
+
   if event then
     if event.type == 'receive' then
-      state = parse(event.data)
+      table.insert(state, parse(event.data))
+
+      if #state >= 4 then
+        currentState = state[1]
+      end
     end
   end
 end
@@ -76,12 +110,12 @@ function love.draw ()
 
   love.graphics.print(peer:round_trip_time(), 0, 0)
 
-  if state[1] then
-    love.graphics.rectangle('fill', tonumber(state[1]), tonumber(state[2]), 10, 10)
+  if currentState and currentState[1] then
+    love.graphics.rectangle('fill', tonumber(currentState[1]), tonumber(currentState[2]), 10, 10)
   end
 
-  if state[3] then
-    love.graphics.rectangle('fill', tonumber(state[3]), tonumber(state[4]), 10, 10)
+  if currentState and currentState[3] then
+    love.graphics.rectangle('fill', tonumber(currentState[3]), tonumber(currentState[4]), 10, 10)
   end
 end
 
